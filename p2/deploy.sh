5cc63b45254d24c9612660839520e096a18f14b7
Jonah Blumstein <jonahblumstein@Jonahs-MacBook-Pro-2.local>
2021-02-21 15:12:38 -0500

    clean up p2

--- p2/deploy.sh
+++ p2/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sam build -t ./template.yaml
-sam deploy --stack-name hw2p2teststack1 --region us-east-1 --s3-bucket jonah-blumstein --capabilities CAPABILITY_IAM
+sam deploy --stack-name hw2p2stack --region us-east-1 --s3-bucket jonah-blumstein --capabilities CAPABILITY_IAM
