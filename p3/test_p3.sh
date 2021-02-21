68661a6b33f12a8473d948ba05e096c77096e37c
Jonah Blumstein <jonahblumstein@Jonahs-MacBook-Pro-2.local>
2021-02-21 16:06:46 -0500

    remove acct number

--- p3/test_p3.sh
+++ p3/test_p3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws sns publish \
-    --topic-arn "arn:aws:sns:us-east-1:806930896523:hw2p3snstopic" \
+    --topic-arn "arn:aws:sns:us-east-1:YOUR_ACCOUNT_NUMBER:hw2p3snstopic" \
     --message "Jonah Andrew Blumstein"
