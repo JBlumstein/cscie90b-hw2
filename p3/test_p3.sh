5d32c5f0d720a28b16a7efd2b172366fff8df9a7
Jonah Blumstein <jonahblumstein@Jonahs-MacBook-Pro-2.local>
2021-02-21 22:06:39 -0500

    p3 working now

--- p3/test_p3.sh
+++ p3/test_p3.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 aws sns publish \
-    --topic-arn "arn:aws:sns:us-east-1:YOUR_ACCOUNT_NUMBER:hw2p3snstopic" \
+    --topic-arn "arn:aws:sns:us-east-1:${SourceAccount}:hw2p3snstopic" \
     --message "Jonah Andrew Blumstein"
+aws sns publish \
+    --topic-arn "arn:aws:sns:us-east-1:${SourceAccount}:hw2p3snstopic" \
+    --message "Jeni Meredith Morrison"
+aws sns publish \
+    --topic-arn "arn:aws:sns:us-east-1:${SourceAccount}:hw2p3snstopic" \
+    --message "Ole Boycat Morrison-Blumstein"
+aws sns publish \
+    --topic-arn "arn:aws:sns:us-east-1:${SourceAccount}:hw2p3snstopic" \
+    --message "RinTinTin Blumstein"
